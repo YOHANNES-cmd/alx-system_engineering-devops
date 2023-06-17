9af1c1bb6e32b00f961213c1fb5b0ff547159dd6
Yohannes Kanko <johanneskanko72gmail.com>
2023-06-17 08:35:13 +0000

    1. User limit

---
+++ 0x1B-web_stack_debugging_4/1-user_limit.pp
@@ -0,0 +1,7 @@
+# Puppet manifest to change the file limit configuration for the holberton user
+
+exec { 'change-os-configuration-for-holberton-user':
+  command => 'sed -iE "s/^holberton hard nofile 5/holberton hard nofile 88888/" /etc/security/limits.conf && sed -iE "s/^holberton soft nofile 4/holberton soft nofile 88888/" /etc/security/limits.conf',
+  path    => '/usr/bin:/usr/sbin:/bin',
+}
+
